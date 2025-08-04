49c99b6e41578b144de28edbeaedc3196bf57df1
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 00:56:45 +0530

    Create To check whether a number is even or odd.cpp

---
+++ To check whether a number is even or odd.cpp
@@ -0,0 +1,18 @@
+//Prathyush Nimmagadda
+//24070123064
+//ENTC A3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int j;
+    cout << "Enter the number: ";
+    cin >> j;
+    if (j % 2 == 0) {
+        cout << "The given number is even." << endl;
+    } else {
+        cout << "The given number is odd." << endl;
+    }
+
+    return 0;
+}
